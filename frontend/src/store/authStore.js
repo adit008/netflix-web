2f20decd0c1d93a06a1bac08ffefc963594a68a9
Aditya Rathore <147158309+adit008@users.noreply.github.com>
2025-06-09 15:54:08 +0530

    Update authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://netflix-web-vilj.onrender.com";
 
 export const useAuthStore = create((set) => ({
   // initial states
