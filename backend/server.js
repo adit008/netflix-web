aac50ea69ed411dee2eb217753d6be593a97c918
Aditya Rathore <147158309+adit008@users.noreply.github.com>
2025-06-09 15:43:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: "http://localhost:5173", credentials: true}))
+app.use(cors({origin: "https://netflix-web2.onrender.com", credentials: true}))
 
 app.get("/", (req, res) => {
   res.send("Welcome To Netflix World!");
